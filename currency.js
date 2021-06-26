8a65b95345ad7424a5567fb7196f920d8f737490
Frances Annor Boakye <francesannorboakye323@gmail.com>
2021-06-26 23:09:31 +0000

    added convert currncy function

--- currency.js
+++ currency.js
@@ -1,4 +1,9 @@
-const crrncy = { EUR: { PLN: 4.15, USD: 0.83 }, USD: { PLN: 3.45, EUR: 1.2 } };
+const crrncy = {
+  EUR: { PLN: 4.15, USD: 0.83 },
+
+  USD: { PLN: 3.45, EUR: 1.2 },
+};
+
 const btn = document.querySelector(".calculate-btn");
 const baseCurrencyInput = document.getElementById("currency-1");
 const secondCurrencyInput = document.getElementById("currency-2");
@@ -7,3 +12,25 @@ const toShowAmount = document.querySelector(".given-amount");
 const toShowBase = document.querySelector(".base-currency");
 const toShowSecond = document.querySelector(".second-currency");
 const toShowResult = document.querySelector(".final-result");
+
+function convertCurrency(event) {
+  event.preventDefault();
+  const amount = amountInput.value;
+  const from = baseCurrencyInput.value;
+  const to = secondCurrencyInput.value;
+  const result = 0;
+
+  try {
+    if (from == to) {
+      result = amount;
+    } else {
+      result = amount * crrncy[from][to];
+    }
+  } catch (err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + " = ";
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result;
+}

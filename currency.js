ac0bb40ce68fd5513841eee02e3aef83c28d7bce
Frances Annor Boakye <francesannorboakye323@gmail.com>
2021-06-26 23:35:46 +0000

    function added

--- currency.js
+++ currency.js
@@ -1,9 +1,3 @@
-const crrncy = {
-  EUR: { PLN: 4.15, USD: 0.83 },
-
-  USD: { PLN: 3.45, EUR: 1.2 },
-};
-
 const btn = document.querySelector(".calculate-btn");
 const baseCurrencyInput = document.getElementById("currency-1");
 const secondCurrencyInput = document.getElementById("currency-2");
@@ -20,18 +14,27 @@ function convertCurrency(event) {
   const to = secondCurrencyInput.value;
   const result = 0;
 
-  try {
-    if (from == to) {
-      result = amount;
-    } else {
-      result = amount * crrncy[from][to];
-    }
-  } catch (err) {
-    result = amount * (1 / crrncy[to][from]);
-  }
   toShowAmount.innerHTML = amount;
   toShowBase.textContent = from + " = ";
   toShowSecond.textContent = to;
   toShowResult.textContent = result;
 }
+
+function calculateNewAmount(fromUnit, fromValue, toUnit) {
+  const crrncy = {
+    EUR: { PLN: 4.15, USD: 0.83 },
+    USD: { PLN: 3.45, EUR: 1.2 },
+  };
+
+  if (fromUnit == toUnit) {
+    return fromUnit;
+  }
+
+  if (crrncy[fromUnit]) {
+    return fromUnit * crrncy[fromUnit][toUnit];
+  }
+
+  return fromUnit * (1 / crrncy[toUnit][fromValue]);
+}
+
 btn.addEventListener("click", convertCurrency);

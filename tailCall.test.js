6bc9b1c653cc3c4c76e219820caeec1a2b8182dc
Frances Annor Boakye <francesannorboakye323@gmail.com>
2021-07-26 21:57:12 +0000

    added a unit test for the tail call

--- tailCall.test.js
+++ tailCall.test.js
@@ -1,6 +1,10 @@
-const recursiveFactorial  = require("./tailCall");
+const recursiveFactorial = require("./tailCall");
 test("returns the factorial of 10", () => {
-    expect(recursiveFactorial(10)).toBe(3628800);
+  expect(recursiveFactorial(10)).toBe(3628800);
 });
 
- 
+for (let i = 0; i < 10; i++) {
+console.log("the number is +1");
+
+
+}
